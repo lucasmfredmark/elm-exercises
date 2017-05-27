db74462db5f757bf468b2fef8777403f84ea3123
Lucas Meulengracht Fredmark <lucas.m.fredmark@gmail.com>
2017-05-27 14:46:59 +0200

    Added solution for chat exercise

--- chat/chat.elm
+++ chat/chat.elm
@@ -3,31 +3,34 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import WebSocket
 import List
+import Json.Decode as Decode
+import Json.Encode as Encode
 
-main: Program Never Model Msg
+main : Program Never Model Msg
 main =
   Html.program
-  { init = init
-  , view = view
-  , update = update
-  , subscriptions = subscriptions
-  }
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
 
- -- MODEL
+-- MODEL
 type alias Model =
-  { chatMessage : String
+  { chatMessage : List String
   , userMessage : String
+  , loginName : String
   }
 
 init : (Model, Cmd Msg)
 init =
-  ( Model "" ""
+  ( Model [""] "" ""
   , Cmd.none
   )
 
 type alias ChatMessage =
-  { command: String
-  , content: String
+  { command : String
+  , content : String
   }
 
 -- UPDATE
@@ -35,6 +38,8 @@ type Msg
   = PostChatMessage
   | UpdateUserMessage String
   | NewChatMessage String
+  | LoginUser
+  | LoginMessage String
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -43,28 +48,54 @@ update msg model =
       let
         message = model.userMessage
       in
-        { model | userMessage = "" } ! [WebSocket.send "ws://localhost:3000/" message]
+        { model | userMessage = "" } ! [WebSocket.send "ws://localhost:3000" message]
 
     UpdateUserMessage message ->
       { model | userMessage = message } ! []
 
     NewChatMessage message ->
-        { model | chatMessage = message } ! []
+      { model | chatMessage = jsonToString (Decode.decodeString (Decode.field "content" Decode.string) message) :: model.chatMessage } ! []
+
+    LoginUser ->
+      let
+        login = Encode.object [("command", Encode.string "login"), ("content", Encode.string model.loginName)]
+      in
+        { model | loginName = "" } ! [WebSocket.send "ws://localhost:3000" (Encode.encode 0 login)]
+
+    LoginMessage message ->
+      { model | loginName = message } ! []
+
+jsonToString : Result String String -> String
+jsonToString result =
+  case result of
+    Ok result -> result
+    Err _ -> "Error"
 
 -- VIEW
 view : Model -> Html Msg
 view model =
   div []
-    [ input [ placeholder "message..."
+    [ input [ placeholder "Login..."
+            , autofocus True
+            , value model.loginName
+            , onInput LoginMessage
+            ] []
+    , button [ onClick LoginUser ] [ text "Login" ]
+    , div [] []
+    , input [ placeholder "Message..."
             , autofocus True
             , value model.userMessage
             , onInput UpdateUserMessage
             ] []
     , button [ onClick PostChatMessage ] [ text "Submit" ]
-    , div [] [ text model.chatMessage ]
-  ]
+    , div [] (List.map showMessage model.chatMessage)
+    ]
 
- -- SUBSCRIPTIONS
+showMessage : String -> Html msg
+showMessage message =
+  div [] [ text message ]
+
+-- SUBSCRIPTIONS
 subscriptions : Model -> Sub Msg
 subscriptions model =
   WebSocket.listen "ws://localhost:3000" NewChatMessage
